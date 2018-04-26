26fe75efbf261470d3fbb0bdf12e1c2b2d7406c6
hemmy6894 <37750218+hemmy6894@users.noreply.github.com>
2018-04-26 11:16:51 +0300

    - First Published Coded app in
    https://drive.google.com/open?id=12trvgWXy1jm70ypmuNFQ7Hr_Qu3qpwai
    
    You cxan view live thanks.

--- app/src/main/java/com/tanzania/comtech/msafiriapp/Payment/PaymentNormal.java
+++ app/src/main/java/com/tanzania/comtech/msafiriapp/Payment/PaymentNormal.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.tanzania.comtech.msafiriapp.Helpers.SharedPreferenceAppend;
 import com.tanzania.comtech.msafiriapp.Helpers.ThanksActivity;
 import com.tanzania.comtech.msafiriapp.R;
 
 public class PaymentNormal extends AppCompatActivity implements View.OnClickListener {
 
     Button nexttopay;
+    EditText phoneNumber;
     boolean isFirstStage = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class PaymentNormal extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_payment_normal);
         new TextToPayFor(getApplicationContext(),PaymentNormal.this);
 
+        phoneNumber = (EditText)findViewById(R.id.payment_normal_phone_number);
+        phoneNumber.setText(new SharedPreferenceAppend(getApplicationContext()).readSharedPref(getString(R.string.shared_preference_session)).get(getString(R.string.shared_phone_number)));
         nexttopay = (Button)findViewById(R.id.payment_normal_button_next);
         nexttopay.setOnClickListener(this);
     }
@@ -27,11 +32,16 @@ public class PaymentNormal extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         if (isFirstStage) {
             setContentView(R.layout.activity_payment_token);
+            Button ok = (Button)findViewById(R.id.payment_token_button_next);
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), ThanksActivity.class);
+                    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            });
             isFirstStage = false;
-        }else {
-            Intent intent = new Intent(getApplicationContext(), ThanksActivity.class);
-            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
         }
     }
 

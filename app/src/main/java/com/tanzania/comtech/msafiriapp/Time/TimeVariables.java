4f0948cc2eb990db6440e1e961bf137888385784
mark p <markmayalla@gmail.com>
2018-08-06 10:00:43 +0300

    Worked with Choose Route Interface and introduced a slideshow

--- app/src/main/java/com/tanzania/comtech/msafiriapp/Time/TimeVariables.java
+++ app/src/main/java/com/tanzania/comtech/msafiriapp/Time/TimeVariables.java
@@ -6,5 +6,5 @@ package com.tanzania.comtech.msafiriapp.Time;
 
 public class TimeVariables {
     public final static String monthNames[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",""};
-    public final static String weeksNames[] = {"Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    public final static String weeksNames[] = {"Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 }
